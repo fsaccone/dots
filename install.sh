24a80d9ed4c7c3904403850c7c47547b94b35df5
Francesco Saccone <francesco@francescosaccone.com>
2025-07-02 21:53:55 +0200

    feat: add .mkshrc
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 source_directory="$(cd "$(dirname "$0")" && pwd)"
-dotfiles="config xinitrc"
+dotfiles="config mkshrc xinitrc"
 
 if [ ! -d "$HOME" ]; then
 	echo "Error: HOME directory does not exist."
